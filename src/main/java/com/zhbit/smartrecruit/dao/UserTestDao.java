408c9386dc31385e7bd8df8a492fd93395e37da3
linchuhao <370356141@qq.com>
2020-03-01 16:13:12 +0800

    Delete redundant test code.

--- src/main/java/com/zhbit/smartrecruit/dao/UserTestDao.java
+++
@@ -1,7 +0,0 @@
-package com.zhbit.smartrecruit.dao;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.zhbit.smartrecruit.data.entity.UserTestEntity;
-
-public interface UserTestDao extends BaseMapper<UserTestEntity> {
-}
